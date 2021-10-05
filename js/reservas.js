83340720c012f6fef05f428911ab6056f1b70e71
jhonmendex <jhonmendex@gmail.com>
2021-10-05 15:54:53 -0500

    change add email confirmation

--- js/reservas.js
+++ js/reservas.js
@@ -1,48 +1,48 @@
 $(document).ready(function () {
-    $("#datepicker_llegada").datepicker();
-    $("#datepicker_salida").datepicker();
-  
-    $("#formulario").on("submit", function (event) {
-      event.preventDefault();
-  
-      let evento = $("#evento").val();
-      let fecha = $("#fecha").val();
-      let hora = $("#hora").val();
-      let numPersonas = $("#numPersonas").val();
-      let nombreCliente = $("#nombreCliente").val();
-      let correoCliente = $("#correoCliente").val();
-      let celularCliente = $("#celularCliente").val();
-      let solicitudCliente = $("#solicitudCliente").val();
-  
-      localStorage.setItem("evento", evento);
-      localStorage.setItem("fecha", fecha);
-      localStorage.setItem("hora", hora);
-      localStorage.setItem("numPersonas", numPersonas);
-      localStorage.setItem("nombreCliente", nombreCliente);
-      localStorage.setItem("correoCliente", correoCliente);
-      localStorage.setItem("celularCliente", celularCliente);
-      localStorage.setItem("solicitudCliente", solicitudCliente);
-  
-      window.location.replace("confirmacion.html");
-    });
+  $("#datepicker_llegada").datepicker();
+  $("#datepicker_salida").datepicker();
+
+  $("#formulario").on("submit", function (event) {
+    event.preventDefault();
+
+    let evento = $("#evento").val();
+    let fecha = $("#fecha").val();
+    let hora = $("#hora").val();
+    let numPersonas = $("#numPersonas").val();
+    let nombreCliente = $("#nombreCliente").val();
+    let correoCliente = $("#correoCliente").val();
+    let celularCliente = $("#celularCliente").val();
+    let solicitudCliente = $("#solicitudCliente").val();
+
+    localStorage.setItem("evento", evento);
+    localStorage.setItem("fecha", fecha);
+    localStorage.setItem("hora", hora);
+    localStorage.setItem("numPersonas", numPersonas);
+    localStorage.setItem("nombreCliente", nombreCliente);
+    localStorage.setItem("correoCliente", correoCliente);
+    localStorage.setItem("celularCliente", celularCliente);
+    localStorage.setItem("solicitudCliente", solicitudCliente);
+
+    window.location.replace("confirmacion.html");
   });
-  
-  function clickEvento(evento) {
-    localStorage.setItem("evento", evento.value);
-  }
-  
-  function clickFecha(fecha) {
-    localStorage.setItem("fecha", fecha.value);
-  }
-  
-  function clickHora(hora) {
-    localStorage.setItem("hora", hora.value);
-  }
-  
-  function clickPersonas(numPersonas) {
-    localStorage.setItem("numPersonas", numPersonas.value);
-  }
-  
-  function clickSolicitud(solicitudCliente) {
-    localStorage.setItem("solicitudCliente", solicitudCliente.value);
-  }
+});
+
+function clickEvento(evento) {
+  localStorage.setItem("evento", evento.value);
+}
+
+function clickFecha(fecha) {
+  localStorage.setItem("fecha", fecha.value);
+}
+
+function clickHora(hora) {
+  localStorage.setItem("hora", hora.value);
+}
+
+function clickPersonas(numPersonas) {
+  localStorage.setItem("numPersonas", numPersonas.value);
+}
+
+function clickSolicitud(solicitudCliente) {
+  localStorage.setItem("solicitudCliente", solicitudCliente.value);
+}
